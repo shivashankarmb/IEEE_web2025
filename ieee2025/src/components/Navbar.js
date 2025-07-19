61e49b25350e6ffc2cc5f32ed2c4c168c42a74de
shivashankar mb <shivashankarmb33@gmail.com>
2025-07-19 14:25:35 +0530

    Navbar chnages and Home Page

--- ieee2025/src/components/Navbar.js
+++ ieee2025/src/components/Navbar.js
@@ -4,39 +4,28 @@ import './Navbar.css';
 import ieeelogo from '../assets/ieeelogo.jpg';
 
 function Navbar() {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen);
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
     <nav className="navbar">
       <div className="logo">
-        <img src={ieeelogo} alt="IEEE Logo" className="logo-img" />
+        <img src={ieeelogo} alt="IEEE Logo" />
         <span>Electrothon-2025</span>
       </div>
 
-      {/* Desktop Links */}
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/committees">Committees</Link></li>
-        <li><Link to="/participants">For Participants</Link></li>
+      <ul className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
+        <li><Link to="/" onClick={toggleMobileMenu}>Home</Link></li>
+        <li><Link to="/committees" onClick={toggleMobileMenu}>Committees</Link></li>
+        <li><Link to="/participants" onClick={toggleMobileMenu}>For Participants</Link></li>
       </ul>
 
-      {/* Mobile Menu Icon */}
       <button className="mobile-menu-icon" onClick={toggleMobileMenu}>
         ☰
       </button>
-
-      {/* Mobile Links */}
-      {isMobileMenuOpen && (
-        <ul className="nav-links-mobile">
-          <li><Link to="/" onClick={toggleMobileMenu}>Home</Link></li>
-          <li><Link to="/committees" onClick={toggleMobileMenu}>Committees</Link></li>
-          <li><Link to="/participants" onClick={toggleMobileMenu}>For Participants</Link></li>
-        </ul>
-      )}
     </nav>
   );
 }
