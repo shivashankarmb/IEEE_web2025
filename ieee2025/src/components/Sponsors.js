b02340c09f4a9a37d75d7a860070f55e32ba94f2
shivashankar mb <shivashankarmb33@gmail.com>
2025-07-25 23:44:49 +0530

    Sponsors changes

--- ieee2025/src/components/Sponsors.js
+++ ieee2025/src/components/Sponsors.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import './Committees.css'; // Reusing the same CSS styles
+import './Sponsors.css';
+import ieePesLogo from '../assets/ieeelogo.jpg';
+import propureLogo from '../assets/Propure Technologies_Logo.jpg';
 
 const Sponsors = () => {
   return (
-    <div className="committees">
-      <div className="committees-container">
-        <h1 className="page-title">Sponsors</h1>
-        <div className="committee-section">
-          <p style={{ fontSize: '1.2rem', color: '#34495e', textAlign: 'center' }}>
-            Will be updated soon
-          </p>
+    <div className="sponsors">
+      <div className="sponsors-container">
+        <h1 className="sponsors-title">Sponsors</h1>
+        <div className="sponsors-logos">
+          <div className="sponsor-card">
+            <div className="sponsor-image">
+              <img src={ieePesLogo} alt="IEEE PES Company" />
+            </div>
+            <p className="sponsor-name">IEEE PES Power & Energy Society</p>
+          </div>
+          <div className="sponsor-card">
+            <div className="sponsor-image">
+              <img src={propureLogo} alt="Propure Technologies" />
+            </div>
+            <p className="sponsor-name">Propure Technologies</p>
+          </div>
         </div>
       </div>
     </div>
